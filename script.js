d94848147302c32ace116bb02f21b7a70971c3cd
Adam Peters <70253021+adam-peters@users.noreply.github.com>
2024-06-13 23:33:48 -0700

    Add Notes for Program Overview

---
+++ script.js
@@ -0,0 +1,15 @@
+// Author: Adam Peters
+// Date: 6/13/24
+// Descr: Rock Paper Scissors Game 
+// Coded in JS
+
+// Get Input
+
+// Generate Computer Response
+// Needs Random Number between One and Three
+
+// Compare Results
+// Same Choice is Tie
+// Rock wins against Scissors
+// Scissors against Paper
+// Paper against Rock
