f0cbf1ec9e90e3f63c78858a8a05f7dec7029203
Adam Peters <70253021+adam-peters@users.noreply.github.com>
2024-06-13 23:47:28 -0700

    Create function for Computer Choice
    Function generates a number between 1-3, then assigns 'Rock', 'Paper',
    'Scissors' to the return string

--- script.js
+++ script.js
@@ -7,9 +7,31 @@
 
 // Generate Computer Response
 // Needs Random Number between One and Three
+function getComputerChoice() {
+    response = "";
+
+    num = parseInt(Math.random() * 3 + 1);
+
+    switch(num) {
+        case 1:
+            response="Rock";
+            break;
+        case 2:
+            response="Scissors";
+            break;
+        case 3:
+            response="Paper";
+            break;
+    }
+
+    return response;
+}
+
+console.log(getComputerChoice());
 
 // Compare Results
 // Same Choice is Tie
 // Rock wins against Scissors
 // Scissors against Paper
 // Paper against Rock
+
